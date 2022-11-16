b6b0142187a68d09cfb4fc9689df4dc0aec6ad38
pluskin23 <pluskin23@gmail.com>
2022-11-16 13:59:58 +0300

    first commit

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamProject_Pilipchuk_Radkevich
+{
+    internal class Person
+    {
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private System.DateTime DateTime { get; set; }
+    }
+}
